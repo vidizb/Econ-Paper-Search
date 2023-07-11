33447472c066eee63d8795f62d11ea2da68935c1
vidizb <107489313+vidizb@users.noreply.github.com>
2023-07-11 19:39:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import re
 st.set_page_config(page_title=None, page_icon=None, layout='centered', initial_sidebar_state='collapsed')
 
 """
-# Econ Paper Search
+# Pencarian Paper JDIH DPRD Batang
 
 """
 
